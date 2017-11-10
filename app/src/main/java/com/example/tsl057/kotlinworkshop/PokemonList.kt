9fee0e75640d29c9715725e350452f894b0b237b
Alex Vidal <arodriguez@tribalscale.com>
2017-11-10 11:15:14 +0400

    initial commit

---
+++ app/src/main/java/com/example/tsl057/kotlinworkshop/PokemonList.kt
@@ -0,0 +1,12 @@
+package com.example.tsl057.kotlinworkshop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PokemonList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_list)
+    }
+}
